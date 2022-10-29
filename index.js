9dd5d46e2bc8752c462a3aa04b122c5b4e2bd762
Caio Gouveia <caiomdm@gmail.com>
2022-10-29 19:17:40 -0200

    signup page

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
 const express = require('express');
+const sqlite3 = require('sqlite3');
+const session = require('express-session');
+const { authenticator } = require('otplib');
+const QRCode = require('qrcode');
+const jwt = require('jsonwebtoken');
+const expressJWt = require('express-jwt');
+
 const bodyParser = require('body-parser');
 const app = express();
 const port  = 3000;
 
 app.set('view engine', 'ejs');
+app.use(session({secret: 'secret'}))
+app.use(bodyParser.urlencoded({extended: false}));
 
+// intial DB create
+const db = new sqlite3.Database('db.sqlite');
+db.serialize(()=>{
+    db.run('CREATE TABLE IF NOT EXISTS `users` (`user_id` INTEGER PRIMARY KEY AUTOINCREMENT, `email` VARCHAR(255) NOT NULL, `secret` varchar(255) NOT NULL)')
+});
+db.close();
+
+
+// sign up page
 app.get("/", (req, res) => {
-  res.render('home.ejs');
+  res.render('signup.ejs');
 });
 
 app.listen(port, () => {

7bbbe161b5683f0c886c063e2059d439a2c284ac
Caio Gouveia <caiomdm@gmail.com>
2022-10-30 14:59:34 -0200

    other endpoints

--- index.js
+++ index.js
@@ -4,14 +4,14 @@ const session = require('express-session');
 const { authenticator } = require('otplib');
 const QRCode = require('qrcode');
 const jwt = require('jsonwebtoken');
-const expressJWt = require('express-jwt');
+const { expressjwt } = require("express-jwt");
 
 const bodyParser = require('body-parser');
 const app = express();
 const port  = 3000;
 
 app.set('view engine', 'ejs');
-app.use(session({secret: 'secret'}))
+app.use(session({secret: 'secret'}));
 app.use(bodyParser.urlencoded({extended: false}));
 
 // intial DB create
@@ -21,6 +21,48 @@ db.serialize(()=>{
 });
 db.close();
 
+function verifyLogin (email, code, req, res, failUrl) {
+    //load user by email
+    console.log('###############################');
+    console.log('VERIFY LOGIN');
+    console.log('###############################');
+    // const db = new sqlite3.Database('db.sqlite')
+    db.serialize(() => {
+      db.get('SELECT secret FROM users WHERE email = ?', [email], (err, row) => {
+        if (err) {
+          throw err
+        }
+
+        if (!row) {
+          console.log('user not found');
+          return res.redirect('/')
+        }
+
+        if (!authenticator.check(code, row.secret)) {
+          console.log("authenticador não valida, secret e code");
+          console.log(JSON.stringify(row));
+          //redirect back
+          return res.redirect(failUrl)
+        }
+
+        //correct, add jwt to session
+        req.session.qr = null
+        req.session.email = null
+        req.session.token = jwt.sign(email, 'secret')
+
+        //redirect to "private" page
+        return res.redirect('/private')
+      });
+    });
+}
+
+const jwtMiddleware = expressjwt({
+    secret: 'supersecret',
+    algorithms: ['HS256'],
+    getToken: (req) => {
+        return req.session.token
+    }
+});
 
 // sign up page
 app.get("/", (req, res) => {
@@ -28,7 +70,7 @@ app.get("/", (req, res) => {
 });
 
 // Sign up post
-app.post('sign-up', (request, response)=>{
+app.post('/sign-up', (request, response)=>{
     const email = request.body.email;
     const secret = authenticator.generateSecret();
     const db = new sqlite3.Database('db.sqlite');
@@ -45,13 +87,58 @@ app.post('sign-up', (request, response)=>{
                 }
                 request.session.qr = url;
                 request.session.email = email;
-                response.redirect('/sign-up-2fs');
+                response.redirect('/sign-up-2fa');
             });
         });
     });
 });
 
+// sign-up-2fa
+app.get('/sign-up-2fa', (req, res) => {
+    if (!req.session.qr) {
+        return res.redirect('/')
+    }
+
+    return res.render('signup-2fa.ejs', { qr: req.session.qr })
+});
+
+// post sign-up-2fa
+app.post('/sign-up-2fa', (req, res) => {
+    if (!req.session.email) {
+        return res.redirect('/')
+    }
+
+    const email = req.session.email,
+        code = req.body.code
+
+    return verifyLogin(email, code, req, res, '/sign-up-2fa')
+});
+
+// login page
+app.get('/login', (req, res) => {
+    return res.render('login.ejs')
+});
+
+// post login
+app.post('/login', (req, res) => {
+    //verify login
+    const email = req.body.email,
+        code = req.body.code
+
+    return verifyLogin(email, code, req, res, '/login')
+});
+
+// Provate pate
+app.get('/private', jwtMiddleware, (req, res) => {
+    return res.render('private.ejs', { email: req.user })
+});
+
+// logout page
+app.get('/logout', jwtMiddleware, (req, res) => {
+    req.session.destroy();
+    return res.redirect('/');
+});
+
 app.listen(port, () => {
-  console.log('Server started.');
-  }
-)
+    console.log('Server started.');
+});
